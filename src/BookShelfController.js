b044d6bedec29277e74ec8fff470c39c51e303d1
Eddie <ekkpodogui@gmail.com>
2021-02-10 04:02:00 +0000

    Add PropTypes

--- src/BookShelfController.js
+++ src/BookShelfController.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class BookShelfController extends Component {
     state = {
@@ -30,5 +31,9 @@ class BookShelfController extends Component {
     }
 }
 
+BookShelfController.propTypes = {
+	book: PropTypes.object.isRequired,
+	onChangeShelf: PropTypes.func.isRequired
+};
 
 export default BookShelfController;
